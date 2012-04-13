61dc147e68be8b15a5518a35a64015301f8caa40
Pascal DAVID <pdavid@ippon.fr>
2012-04-13 13:31:08 +0200

    add test class for selenium

---
+++ src/test/java/com/selenium/tests/SeleniumTest.java
@@ -0,0 +1,33 @@
+package com.selenium.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+
+public class SeleniumTest {
+	private DefaultSelenium selenium;
+	
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://localhost:8080/");
+		selenium.start();
+	}
+
+	@Test
+	public void testSelenium () throws Exception {
+		selenium.open("/jerseySpringJPA/");
+		selenium.type("id=ident", "0");
+		selenium.type("id=nom", "Durand");
+		selenium.type("id=age", "45");
+		selenium.click("css=input[type=\"submit\"]");
+		selenium.waitForPageToLoad("30000");
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+
+}
