55f6d7dbf071f7668faf2a733883671ef4d6c687
Pascal DAVID <pdavid@ippon.fr>
2012-04-18 12:42:36 +0200

    Ajout classe persistante Address

---
+++ src/main/java/com/persistent/entity/Address.java
@@ -0,0 +1,60 @@
+package com.persistent.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "ADDRESS")
+public class Address {
+	
+	
+	private int id;
+	private String voie;
+	private String ville;
+	private int code;
+	protected Person locataire;
+	
+	@OneToOne
+	@JoinColumn(name="ID_PERSON", referencedColumnName = "ID")
+	public Person getLocataire() {
+		return locataire;
+	}
+	public void setLocataire(Person locataire) {
+		this.locataire = locataire;
+	}
+	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getVoie() {
+		return voie;
+	}
+	public void setVoie(String voie) {
+		this.voie = voie;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+}
